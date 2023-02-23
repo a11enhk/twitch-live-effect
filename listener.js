08da90750e2e1ca9ead021ee6602a3928cb65719
a11enhk <121772435+a11enhk@users.noreply.github.com>
2023-02-24 01:50:18 +0800

    feat: vs connect

--- listener.js
+++ listener.js
@@ -4,8 +4,28 @@ const waitSecond = (s) => new Promise((resolve) => {
     }, s * 1000);
 });
 
+Array.prototype.random = function () {
+    return this[Math.floor((Math.random()*this.length))];
+}
+
+$('#word').textfill({
+    maxFontPixels: 200
+});
+
 const queue = [];
 let animating = false;
+
+const wordList1 = [
+    '123',
+    '456',
+    '789',
+];
+
+const wordList2 = [
+    '321',
+    '654',
+    '987',
+];
 
 $(window).on('redemption', async function (e, title, user, input) {
     if (animating) {
@@ -13,18 +33,18 @@ $(window).on('redemption', async function (e, title, user, input) {
         return;
     }
     animating = true;
-    if (title === '請台主幫我安慰我朋友') {
-
-        $("#img1").fadeIn();
-        await waitSecond(30);
-        $("#img1").fadeOut();
-
-    } else {
-
-        $("#img2").fadeIn();
-        await waitSecond(30);
-        $("#img2").fadeOut();
-
+    if (title === '試野') {
+        const word1 = wordList1.random();
+        const word2 = wordList2.random();
+        $("#word span").text(`${user}${word1}${word2}`);
+        $('#word').textfill({
+            maxFontPixels: 200
+        });
+        $("#word-wrapper").fadeIn();
+        await waitSecond(300);
+        $("#word-wrapper").fadeOut();
+    } else if (title === '印度搖') {
+        executeVSAction('india');
     }
     animating = false;
     if (queue.length) {
---
+++ vs.js
@@ -0,0 +1,89 @@
+// https://github.com/DenchiSoft/VTubeStudio
+/*
+1. Goto https://websocketking.com/
+2. connect ws://localhost:8001
+3. send the payload:
+```
+{
+	"apiName": "VTubeStudioPublicAPI",
+	"apiVersion": "1.0",
+	"requestID": "SomeID",
+	"messageType": "AuthenticationTokenRequest",
+	"data": {
+		"pluginName": "My Cool Plugin",
+		"pluginDeveloper": "My Name"
+	}
+}
+```
+5. go back to Vtube Studio and approve.
+6. Copy the authenticationToken and paste to line 22 ->
+ */
+
+var authenticationToken = '';
+var vsWs;
+var hotkeys = {};
+
+function nonce(length) {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (var i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+
+function send(messageType, data) {
+    vsWs.send(JSON.stringify({
+        apiName: 'VTubeStudioPublicAPI',
+        apiVersion: '1.0',
+        requestID: nonce(8),
+        messageType,
+        data }));
+}
+
+function executeVSAction(name) {
+    send('HotkeyTriggerRequest', { hotkeyID: hotkeys[name] });
+}
+
+function vsConnect() {
+    vsWs = new WebSocket('ws://localhost:8001');
+
+    vsWs.onopen = function(event) {
+        setTimeout(() => {
+            send('AuthenticationRequest', {
+                pluginName: 'My Cool Plugin',
+                pluginDeveloper: 'Me',
+                authenticationToken: authenticationToken,
+            })
+        }, 3000);
+    };
+    vsWs.onmessage = function(e) {
+        const event = JSON.parse(e.data);
+        if (event.messageType === 'AuthenticationResponse') {
+            send('CurrentModelRequest');
+        } else if (event.messageType === 'CurrentModelResponse') {
+            send('HotkeysInCurrentModelRequest', {
+                modelID: event.data.modelID
+            });
+        } else if (event.messageType === 'HotkeysInCurrentModelResponse') {
+            event.data.availableHotkeys.forEach(element => {
+                hotkeys[element.name] = element.hotkeyID;
+            });
+        }
+    };
+
+    vsWs.onclose = function(e) {
+        console.log('Socket is closed. Reconnect will be attempted in 1 second.', e.reason);
+        setTimeout(function() {
+            vsConnect();
+        }, 1000);
+    };
+    
+    vsWs.onerror = function(err) {
+        console.error('Socket encountered error: ', err.message, 'Closing socket');
+        ws.close();
+    };
+
+}
+
+$(function() { vsConnect(); });
